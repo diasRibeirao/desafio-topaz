6d88ade52fa17e0f9afc17056970ea4160cb116f
Emerson Dias <emerson@bmgtecnologia.com.br>
2026-08-20 10:01:20 -0300

    chore: configura persistencia JPA com H2

---
+++ src/main/java/br/com/topaz/encurtador/repository/UrlCurtaJpaRepository.java
@@ -0,0 +1,55 @@
+package br.com.topaz.encurtador.repository;
+
+import br.com.topaz.encurtador.domain.UrlCurta;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped
+public class RepositorioUrlCurtaJpa {
+
+    @PersistenceContext(unitName = "urlEncurtadorPU")
+    private EntityManager entityManager;
+
+    public void save(UrlCurta urlCurta) {
+        entityManager.persist(urlCurta);
+    }
+
+    public Optional<UrlCurta> findByCodido(String codigo) {
+        List<UrlCurta> resultado =
+                entityManager
+                        .createQuery(
+                                "SELECT u " +
+                                        "FROM UrlCurta u " +
+                                        "WHERE u.codigo = :codigo",
+                                UrlCurta.class
+                        )
+                        .setParameter("codigo", codigo)
+                        .setMaxResults(1)
+                        .getResultList();
+
+        if (resultado.isEmpty()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(resultado.get(0));
+    }
+
+    public boolean existsByCodido(String codigo) {
+        Long quantidade =
+                entityManager
+                        .createQuery(
+                                "SELECT COUNT(u) " +
+                                        "FROM UrlCurta u " +
+                                        "WHERE u.codigo = :codigo",
+                                Long.class
+                        )
+                        .setParameter("codigo", codigo)
+                        .getSingleResult();
+
+        return quantidade > 0;
+    }
+}
